50742696282f9025e81cee568b304468e76126d4
Isaac684 <hc21018@ues.edu.sv>
2023-06-22 23:51:52 -0600

    Arreglo de bug visual

--- myAppPage/myApp/urls.py
+++ myAppPage/myApp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.login),
     path('homepage', views.homepage),
     path('loginapp', views.login),
     path('signupapp', views.signup),
