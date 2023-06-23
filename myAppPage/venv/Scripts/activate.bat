ab53b8d2e7c1499a62fdb39185f463cb1a77137f
Isaac684 <hc21018@ues.edu.sv>
2023-06-22 22:26:29 -0600

    Cambios de diseño

--- myAppPage/venv/Scripts/activate.bat
+++ myAppPage/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\luis3\Documents\repository\myApp-djangoproject\DiferenciasDivPagina\myAppPage\venv
+set VIRTUAL_ENV=C:\Users\Isaac\DiferenciasDivPagina\myAppPage\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
