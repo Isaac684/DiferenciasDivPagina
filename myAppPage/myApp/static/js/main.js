04e97ffde3263bfd0bc137223125489c61ec620d
Isaac684 <hc21018@ues.edu.sv>
2023-06-24 10:38:16 -0600

    Validacion de inputs de datos

--- myAppPage/myApp/static/js/main.js
+++ myAppPage/myApp/static/js/main.js
@@ -1,37 +1,89 @@
 const buttonAceptar = document.querySelector('#button-aceptar')
 const inputPuntos = document.querySelector('#cantidad-puntos')
 const ingresarXY = document.querySelector('.puntosXY')
-let puntoIngresado = false;
 
 
 inputPuntos.addEventListener('keypress', function (e) {
-    var key = e.which || e.keyCode; // Obtener el código de tecla presionada
+    let key = e.which || e.keyCode; // Obtener el código de tecla presionada
+    if(!inputPuntos.value == ''){
+        ingresarXY.innerHTML = `
+        <label class="form-label">Ingrese el valor para el punto</label>
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" disabled placeholder="Valor de x">
+            <input type="text" class="form-control" disabled placeholder="Valor de y">
+        </div>`
+    }    
     //Verificar si el código de tecla no corresponde a un número
     if (key < 48 || key > 57) {
+        ingresarXY.innerHTML = `
+            <label class="form-label">Ingrese el valor para el punto</label>
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" disabled placeholder="Valor de x">
+                <input type="text" class="form-control" disabled placeholder="Valor de y">
+            </div>`
         e.preventDefault(); // Evitar la acción predeterminada (no ingresar el carácter)
     }
+})
 
-    //este codigo sera para cuando se ingresen los valores de X y Y
-    // if ((key < 48 || key > 57) && key !== 46 || (key === 46 && puntoIngresado)) {
-    //     e.preventDefault(); // Evitar la acción predeterminada (no ingresar el carácter)
-    // }
-    // // Si se presionó el punto y no se ha ingresado previamente, marcar como ingresado
-    // if (key === 46 && !puntoIngresado) {
-    //     puntoIngresado = true;
-    // }
+inputPuntos.addEventListener('keyup', function (e) {
+    let key = e.which || e.keyCode; // Obtener el código de tecla presionada
+    if(!inputPuntos.value == ''){
+        ingresarXY.innerHTML = `
+        <label class="form-label">Ingrese el valor para el punto</label>
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" disabled placeholder="Valor de x">
+            <input type="text" class="form-control" disabled placeholder="Valor de y">
+        </div>`
+    }    
+    //Verificar si el código de tecla no corresponde a un número
+    if (key < 48 || key > 57) {
+        ingresarXY.innerHTML = `
+            <label class="form-label">Ingrese el valor para el punto</label>
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" disabled placeholder="Valor de x">
+                <input type="text" class="form-control" disabled placeholder="Valor de y">
+            </div>`
+        e.preventDefault(); // Evitar la acción predeterminada (no ingresar el carácter)
+    }
 })
 
 buttonAceptar.addEventListener('click',function () {
     cantPuntos = inputPuntos.value
     if (cantPuntos != '') {
-        ingresarXY.innerHTML= ''
-        for (let i = 0; i < cantPuntos; i++) {
-            ingresarXY.innerHTML += `
-            <label class="form-label">Ingrese el valor para el punto ${i+1}:</label>
-            <div class="input-group mb-3" id="grupoPuntos${i+1}">
-                <input type="text" class="form-control" placeholder="Valor de x${i+1}">
-                <input type="text" class="form-control" placeholder="Valor de y${i+1}">
-            </div>`
+        if (cantPuntos<=100 && cantPuntos>2) {            
+            ingresarXY.innerHTML= ''
+            for (let i = 0; i < cantPuntos; i++) {
+                ingresarXY.innerHTML += `
+                <label class="form-label">Ingrese el valor para el punto ${i+1}:</label>
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control valorX" placeholder="Valor de x${i+1}">
+                    <input type="text" class="form-control valorY" placeholder="Valor de y${i+1}">
+                </div>`
+            }
         }
     }
+
+    let  valoresX = document.querySelectorAll('.valorX');
+    let valoresY = document.querySelectorAll('.valorY');
+    eventoInputsXY(valoresX)
+    eventoInputsXY(valoresY)
 })
+
+const eventoInputsXY = (valores) =>{
+    for (var i = 0; i < valores.length; i++) {
+        valores[i].addEventListener('keypress', function(e) {
+            var key = e.which || e.keyCode; // Obtener el código de tecla presionada
+            var inputValue = this.value // Obtener el valor actual del campo
+    
+            // Verificar si el código de tecla no corresponde a un número ni al punto
+            if ((key < 48 || key > 57) && key !== 46) {
+            e.preventDefault() // Evitar la acción predeterminada (no ingresar el carácter)
+            }
+    
+            // Verificar si ya hay un punto en el valor del campo
+            if (inputValue.indexOf('.') !== -1 && key === 46) {
+            e.preventDefault() // Evitar la acción predeterminada (no ingresar el segundo punto)
+            }
+        })
+    }
+}
